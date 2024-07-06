ee97d9ec19ed821489643e8c6d5437f39422eda7
Ana Beatriz Cabral Pinto <beatrizcabralp.2002@gmail.com>
2024-07-06 01:45:46 -0300

    inicio do carrossel :)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import iniciarCarrossel from "./carrossel-servicos.js"
+// import iniciarModal from "./sevicos-modal.js";
+
+import initServicoModal from "./sevicos-modal.js";
+
+import initScrollSuave from "./scroll-suave.js";
+
+iniciarCarrossel();
+// iniciarModal();
+
+initServicoModal();
+
+initScrollSuave();
+
+
