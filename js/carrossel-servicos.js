62ac050af03818cb86248c6a48a8005ddfde2b08
Ana Beatriz Cabral Pinto <beatrizcabralp.2002@gmail.com>
2024-07-07 23:22:22 -0300

    commitando

--- js/carrossel-servicos.js
+++ js/carrossel-servicos.js
@@ -4,38 +4,24 @@ export default function iniciarCarrossel ()
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    slidesPerView: 2.7,
+    spaceBetween: 20,
+  
+    // If we need pagination
+   
   
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
-    breakpoints: {
-
-      1200: {
-        slidesPerView: 2.7,
-        spaceBetween: 20
-      },
-      
-      900: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      
-      600: {
-        slidesPerView: 1.5,
-        spaceBetween: 10,
-      },
-
-      300: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      }
-    },
+  
+    // And if we need scrollbar
+    
+  });
 
   
-});
+
 }
 
 
